0e0448954b50f69a5f233c2fdd37495f346f133f
hiromi-nagahata <hiromi.nagahata740428@gmail.com>
2026-05-11 22:07:17 +0900

    feat(slick): スライダーの自動再生機能を追加

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// $(".sv").slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
+
+$(".sv").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  //   speed: 3000,
+  // fade: true,
+});
